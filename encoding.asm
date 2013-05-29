5f1819ca69d599c35920069a394cb5caf68a6f23
bullet1a <am-hs@Live.com>
2013-05-29 14:54:10 +0300

    Update encoding.asm

--- encoding.asm
+++ encoding.asm
@@ -1,3 +1,4 @@
+;a program that encodes a user input word into a number and print it
 include 'win32ax.inc'
    str1 dw 20 dup (?)
    str2 dw 01,17,18,19,20,21,22,23
@@ -6,31 +7,32 @@ include 'win32ax.inc'
            ,15,16
    str3 dw 20 dup(?)
   .code
-  start :
- mov si,offset str1
- mov di,offset str3
- mov bx,offset str2
- mov dx,offset str1
- mov ah,0ah
- int 21h
- mmov si,offset str1
- mov cx,00
- bach:mov al,[si]
- cmp al,61h
- jae test2
- cmp al,41h
- jae test3
- jmp exit
- test2:cmp al,7ah
- jbe dl
- jmp exit
- test3:cmp al,5ah
- jbe dll
- jmp exit
- dl:sub al,60h
- jmp ll
- dll:sub al,40h
- jmp ll
+  start :                                            13 w
+ mov si,offset str1                                  14 x
+                                                     15 y
+ mov di,offset str3                                  16 z
+ mov bx,offset str2                                  17 a
+ mov dx,offset str1                                  18 b
+ mov ah,0ah                                          19 c
+ int 21h                                             20 d
+ mmov si,offset str1                                 21 e
+ mov cx,00                                           22 f
+ bach:mov al,[si]                                    23 g
+ cmp al,61h                                          24 h
+ jae test2                                           25 i
+ cmp al,41h                                          26 j
+ jae test3                                           27 k
+ jmp exit                                            28 l
+ test2:cmp al,7ah                                    29 m
+ jbe dl                                              30 n
+ jmp exit                                            31 o
+ test3:cmp al,5ah                                    32 p
+ jbe dll                                             33 q
+ jmp exit                                            34 r
+ dl:sub al,60h                                       35 s
+ jmp ll                                              36 t
+ dll:sub al,40h                                      37 u
+ jmp ll                                              38 v
  ll:cmp al,09h
  ja dt
  mov al,cx
